a5fb323c4571b6950e34ab9765f5376dd2f0e26b
gzukowskiwork@gmail.com <gzukowskiwork@gmail.com>
2021-01-04 18:22:09 +0100

    weather popup shows coordinates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { WeatherPopupComponent } from './weather-popup/weather-popup.component';
+import { WeatherPopupComponent } from './Weather/weather-popup/weather-popup.component';
 import { WorldMapComponent } from './world-map/world-map.component';
+import { HeaderComponent } from './header/header.component';
+import { WeatherForecastComponent } from './Weather/weather-forecast/weather-forecast.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WeatherPopupComponent,
-    WorldMapComponent
+    WorldMapComponent,
+    HeaderComponent,
+    WeatherForecastComponent
   ],
   imports: [
     BrowserModule
--- src/app/world-map/world-map.component.ts
+++ src/app/world-map/world-map.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import {OSM} from 'ol/source';
+import {degreesToStringHDMS, toStringHDMS} from 'ol/coordinate';
+import {toLonLat} from 'ol/proj';
 
 @Component({
   selector: 'app-world-map',
@@ -19,6 +21,9 @@ export class WorldMapComponent implements OnInit {
   }
 
   initializeMap(): void{
+    const container = document.getElementById('popup');
+    const content = document.getElementById('popup-content');
+    const closer = document.getElementById('popup-closer');
     this.map = new Map({
       layers: [
         new TileLayer({
@@ -30,5 +35,15 @@ export class WorldMapComponent implements OnInit {
         zoom: 2
       }),
     });
+    this.map.on('singleclick', function(evt) {
+      const coordinate = evt.coordinate;
+      const hdms = toStringHDMS(toLonLat(coordinate));
+
+      content.innerHTML = '<p> current coordiantes are: </p> <code>' + hdms + '</code>';
+    });
+
+
   }
+
+
 }
